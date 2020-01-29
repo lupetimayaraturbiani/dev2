0895845f52145795f52e494c01e0ed497446aa9f
Mayara Lupeti Turbiani <49393984808@info.local>
2020-01-29 17:25:59 -0300

    pastas dia 29/01

--- Biblioteca_tarde.sql
+++ Biblioteca_tarde.sql
@@ -1,3 +1,5 @@
+-- DDL
+
 -- CRIAR BANCO DE DADOS
 CREATE DATABASE Biblioteca_Tarde;
 
@@ -47,3 +49,29 @@ DROP COLUMN Descricao;
  -- EXCLUIR BANCO DE DADOS
  DROP DATABASE Biblioteca_Tarde;
 
+
+ -- DML
+ INSERT INTO Autores (NomeAutor)
+ VALUES ('Sarah J Maas'), ('Kiera Cass'), ('Tracy Banghart'), (' J. R. R. Tolkien'), (' William P. Young');
+
+  INSERT INTO Generos (Nome)
+ VALUES ('Aventura'), ('Romance'), ('Ficção'), ('Suspense'), ('Fantasia');
+
+  INSERT INTO Livros (Titulo, IdAutor, IdGenero)
+ VALUES ('Trono de Vidro', 1, 3),
+        ('Graça e Fúria', 3, 1),
+		('O Senhor Dos Anéis', 4, 5),
+		('A Seleção', 2, 2),
+		('A Cabana', 5, 4);
+
+
+UPDATE Generos
+SET Nome = 'Aventura'
+WHERE IdGenero = '1';
+
+UPDATE Livros
+SET IdGenero = '3'
+WHERE IdLivro = '2';
+
+DELETE FROM Autores
+WHERE IdAutor = 4;
--- Optus_tarde.sql
+++ Optus_tarde.sql
@@ -1,39 +1,108 @@
+ --DDL Linguagem de definiçao de dados
+
+ --Criar o banco de dados
 CREATE DATABASE Optus_tarde;
 
 USE Optus_tarde;
 
 CREATE TABLE Artistas (
-	IdArtistas INT PRIMARY KEY IDENTITY,
-	Nome VARCHAR(200) NOT NULL
+	IdArtistas		INT PRIMARY KEY IDENTITY,
+	Nome			VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE Estilos (
-	IdEstilos INT PRIMARY KEY IDENTITY,
-	Nome VARCHAR(200) NOT NULL
+	IdEstilos		INT PRIMARY KEY IDENTITY,
+	Nome			VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE Albuns (
-	IdAlbuns INT PRIMARY KEY IDENTITY,
-	Nome VARCHAR(200) NOT NULL,
-	DataLancamento INT,
-	IdArtistas INT FOREIGN KEY REFERENCES Artistas (IdArtistas),
-	IdEstilos INT FOREIGN KEY REFERENCES Estilos (IdEstilos)
+	IdAlbuns		INT PRIMARY KEY IDENTITY,
+	Nome			VARCHAR(200) NOT NULL,
+	DataLancamento	DATE,
+	IdArtista		INT FOREIGN KEY REFERENCES Artistas (IdArtistas),
+	IdEstilo		INT FOREIGN KEY REFERENCES Estilos (IdEstilos)
 );
 
 
-
 CREATE TABLE TipoUsuario (
-	IdTipoUsuario INT PRIMARY KEY IDENTITY,
-	Nome VARCHAR(200) NOT NULL
+	IdTipoUsuario	INT PRIMARY KEY IDENTITY,
+	Nome			VARCHAR(200) NOT NULL
 );
 
 
 CREATE TABLE Usuario (
 	IdUsuario INT PRIMARY KEY IDENTITY,
 	Nome VARCHAR(200) NOT NULL,
+	Telefone INT,
+	Endereço VARCHAR(250),
 	IdTipoUsuario INT FOREIGN KEY REFERENCES TipoUsuario (IdTipoUsuario)
 );
 
 
 
+--DML LINGUAGEM DE MANIPULAÇAO DE DADOS
+SELECT * FROM Artistas;
+SELECT * FROM Estilos;
+SELECT * FROM Albuns;
+SELECT * FROM TipoUsuario;
+SELECT * FROM Usuario;
 
+-- COMANDO DE INSERIR DADOS
+INSERT INTO Estilos (Nome)
+VALUES ('Samba'), ('Pagode'), ('MPB'), ('Rock');
+
+INSERT INTO Estilos (Nome)
+VALUES ('Indie');
+
+INSERT INTO Artistas (Nome)
+VALUES ('Zeca Pagodinho'), ('Tiaguinho'), ('Caetano Veloso'), ('Legião Urbana'); 
+
+INSERT INTO Artistas (Nome)
+VALUES ('Imagine Dragons');
+
+INSERT INTO Albuns (Nome, DataLancamento, IdArtista, IdEstilo)
+VALUES ('Eu Cansei', '25/01/2020', 2, 2),
+       ('Voltei Brasil', '30/03/2020', 1, 2),
+	   ('É Agora', '12/10/2020', 3, 3),
+	   ('Tempo Perdido', '07/08/2020', 4, 4);
+
+INSERT INTO Albuns (Nome, DataLancamento, IdArtista, IdEstilo)
+VALUES ('Evolve', '22/10/2017', 5, 5);
+
+
+INSERT INTO TipoUsuario (Nome)
+VALUES ('Comum'), ('Administrador');
+
+INSERT INTO TipoUsuario (Nome)
+VALUES ('Plebeu'), ('Realeza'), ('Escória');
+
+INSERT INTO Usuario (Nome, Telefone, Endereço,  IdTipoUsuario)
+VALUES ('José Bezerra', '11 22334455', 'Rua Rio Branco de Cardoso 23', 1),
+       ('Carla Santos', '11 12345678', 'Av Guarajá 111', 4),
+	   ('Judson Henrrique', '11 09876543', 'Rua dos Palmares 98', 3),
+	   ('Laura Silveira', '11 54327658', 'Rua João Carlos Pereira 543', 2),
+	   ('Silvio Valverde', '11 24567388', 'Av Whashington Luiz 721', 5);
+
+
+-- DELETE: APAGAR DADOS
+DELETE FROM Albuns
+WHERE IdAlbuns = 4;
+
+
+-- UPDATE: ALTERA DADOS
+UPDATE Artistas
+SET Nome = 'Pericles'
+WHERE IdArtistas = 2;
+
+UPDATE TipoUsuario
+SET Nome = 'Não Sei'
+WHERE IdTipoUsuario = 5;
+
+-- NAO TENHO ATRIBUTO VIZUALIZAÇOES: VOU ALTERAR A DATA DE LANÇAMENTO
+UPDATE Albuns
+SET DataLancamento = '06/07/2020'
+WHERE IdAlbuns = 4;
+
+
+-- LIMPAR TODOS OS DADOS DA TABELA
+TRUNCATE TABLE Albuns;
