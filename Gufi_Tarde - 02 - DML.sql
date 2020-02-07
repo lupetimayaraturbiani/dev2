447e556fce29c43643462884d86542d2cb6f0773
Mayara Lupeti Turbiani <49393984808@info.local>
2020-02-07 16:27:48 -0300

    pastas dia 07/02

--- Gufi_Tarde - 02 - DML.sql
+++ Gufi_Tarde - 02 - DML.sql
@@ -2,6 +2,13 @@
 USE Gufi_Tarde;
 
 SELECT * FROM Usuario;
+SELECT * FROM TipoUsuario;
+SELECT * FROM TipoEvento;
+SELECT * FROM Instituicao;
+SELECT * FROM Evento;
+SELECT * FROM Presenca;
+
+
 
 INSERT INTO TipoUsuario (TituloTipoUsuario)
 VALUES ('Administrador'), ('Comum');
@@ -12,6 +19,9 @@ VALUES ('C#'),('React'),('SQL');
 INSERT INTO Instituicao (CNPJ, Nome, Endereco)
 VALUES ('11111111111111', 'Escola SENAI de Informática', 'Alameda Barão de Limeira, 538');
 
+INSERT INTO Instituicao (CNPJ, Nome, Endereco)
+VALUES ('22222222222222', 'SESI - Centro Educacional 388', 'Av.Santa Inês, 225');
+
 INSERT INTO Usuario (NomeUsuario, Email, Senha, Genero, DataNascimento, IdTipoUsuario)
 VALUES ('Administrador', 'adm@gmail.com', 'adm123', 'Não Informado', '06/02/2020', 1),
        ('Carol', 'carol@gmail.com', 'carol123', 'Feminino', '06/02/2020', 2),
@@ -21,3 +31,15 @@ INSERT INTO Evento (NomeEvento, DataEvento, Descricao, AcessoLivre, IdInstituica
 VALUES ('Introducao ao C#', '07/02/2020', 'Conceitos sobre os pilares da programação orientada', 1, 1, 1),
 	   ('Ciclo de Vida', '07/02/2020', 'Como utilizar o ciclo de vida com ReactJs', 0, 1, 2),
 	   ('Optimização de banco de dados', '07/02/2020', 'Aplicação de indices clusterizados e não clusterizados', 1, 1, 3);
+
+UPDATE Usuario
+SET Genero = 'Feminino'
+WHERE IdUsuario = 6;
+
+
+INSERT INTO Presenca (IdUsuario, IdEvento, Situacao)
+VALUES (2, 2, 'Agendada'),
+       (2, 3, 'Confirmada'),
+	   (3, 1, 'Não compareceu');
+
+
---
+++ Gufi_Tarde - 03 - DQL.sql
@@ -0,0 +1,59 @@
+USE Gufi_Tarde;
+
+
+
+-- DQL
+SELECT Usuario.NomeUsuario, Usuario.Email, Usuario.DataNascimento, Usuario.Genero, TipoUsuario.TituloTipoUsuario
+FROM Usuario
+INNER JOIN TipoUsuario ON Usuario.IdTipoUsuario = TipoUsuario.IdTipoUsuario;
+
+SELECT Instituicao.CNPJ, Instituicao.Endereco, Instituicao.Nome FROM Instituicao;
+
+SELECT TipoEvento.TituloTipoEvento FROM TipoEvento;
+
+SELECT Evento.NomeEvento, Evento.DataEvento, Evento.Descricao, Evento.AcessoLivre, 
+TipoEvento.TituloTipoEvento,Instituicao.Nome, Instituicao.Endereco
+FROM Evento
+INNER JOIN TipoEvento ON TipoEvento.IdTipoEvento = Evento.IdTipoEvento
+INNER JOIN Instituicao ON Instituicao.IdInstituicao = Evento.IdInstituicao
+WHERE Evento.AcessoLivre = 1;
+
+SELECT Evento.NomeEvento, Evento.DataEvento, Evento.Descricao, Evento.AcessoLivre, 
+TipoEvento.TituloTipoEvento, Instituicao.Nome, Instituicao.Endereco, Usuario.NomeUsuario
+,Usuario.NomeUsuario, Usuario.Email, Usuario.DataNascimento, Usuario.Genero, 
+TipoUsuario.TituloTipoUsuario, Presenca.Situacao
+FROM Presenca
+INNER JOIN Evento ON Evento.IdEvento = Presenca.IdEvento
+INNER JOIN TipoEvento ON TipoEvento.IdTipoEvento = Evento.IdTipoEvento
+INNER JOIN Instituicao ON Instituicao.IdInstituicao = Evento.IdInstituicao
+INNER JOIN Usuario ON Usuario.IdUsuario = Presenca.IdUsuario
+INNER JOIN TipoUsuario ON TipoUsuario.IdTipoUsuario = Usuario.IdTipoUsuario
+WHERE Usuario.IdUsuario = 2;
+
+SELECT * FROM Presenca;
+
+
+SELECT Evento.NomeEvento, Evento.DataEvento, Evento.Descricao, 
+Instituicao.Nome AS NomeInstituicao,
+CASE 
+	WHEN AcessoLivre = '1' THEN 'Público'
+	WHEN AcessoLivre = '0' THEN 'Privado'
+END AS TipoAcesso
+FROM Evento
+INNER JOIN Instituicao ON Instituicao.IdInstituicao = Evento.IdInstituicao
+INNER JOIN Presenca ON Presenca.IdPresenca = Presenca
+WHERE Presenca.Situacao = 'Confirmada';
+
+SELECT Evento.NomeEvento, Evento.DataEvento, Evento.Descricao, Evento.AcessoLivre, 
+TipoEvento.TituloTipoEvento, Instituicao.Nome, Instituicao.Endereco, Usuario.NomeUsuario
+,Usuario.NomeUsuario, Usuario.Email, Usuario.DataNascimento, Usuario.Genero, 
+TipoUsuario.TituloTipoUsuario, Presenca.Situacao
+FROM Presenca
+INNER JOIN Evento ON Evento.IdEvento = Presenca.IdEvento
+INNER JOIN TipoEvento ON TipoEvento.IdTipoEvento = Evento.IdTipoEvento
+INNER JOIN Instituicao ON Instituicao.IdInstituicao = Evento.IdInstituicao
+INNER JOIN Usuario ON Usuario.IdUsuario = Presenca.IdUsuario
+INNER JOIN TipoUsuario ON TipoUsuario.IdTipoUsuario = Usuario.IdTipoUsuario
+WHERE Usuario.IdUsuario = 2
+AND Presenca.Situacao = 'Confirmada';
+
