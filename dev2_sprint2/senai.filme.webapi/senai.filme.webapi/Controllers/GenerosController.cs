4f0c484e0abda803ff737537573929594c0d5963
Mayara Lupeti Turbiani <49393984808@info.local>
2020-02-17 16:05:37 -0300

    pastas dia 17/02

---
+++ dev2_sprint2/senai.filme.webapi/senai.filme.webapi/Controllers/GenerosController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using senai.filme.webapi.Domains;
+using senai.filme.webapi.Interfaces;
+using senai.filme.webapi.Repositories;
+
+namespace senai.filme.webapi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenerosController : ControllerBase
+    {
+        private IGeneroRepository _generoRepository { get; set; }
+
+        public GenerosController()
+        {
+            _generoRepository = new GeneroRepository();
+
+        }
+
+        [HttpGet]
+        public IEnumerable<GeneroDomain> Get()
+        {
+            return _generoRepository.Listar();
+        }
+
+        [HttpPost]
+        public IActionResult Post(GeneroDomain genero)
+        {
+            _generoRepository.Cadastrar(genero);
+
+            return StatusCode(201);
+        }
+    }
+}
---
+++ dev2_sprint2/senai.filme.webapi/senai.filme.webapi/Interfaces/IGeneroRepository.cs
@@ -0,0 +1,24 @@
+﻿using senai.filme.webapi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace senai.filme.webapi.Interfaces
+{
+    interface IGeneroRepository
+    {
+        /// <summary>
+        /// Listar todos os gêneros
+        /// </summary>
+        /// <returns></returns>
+        List<GeneroDomain> Listar();
+
+        /// <summary>
+        /// Cadastraum novo gênero
+        /// </summary>
+        /// <param name="genero">um gênero</param>
+        void Cadastrar(GeneroDomain genero);
+    }
+}
